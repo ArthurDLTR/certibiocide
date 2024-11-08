7b388fdceb7cf2194d0d392c7e0205d39844c7cc
Atu <arthur.lenoble4@etu.univ-lorraine.fr>
2024-11-08 15:49:14 +0100

    Commit de v1.1

--- core/modules/modCertibiocide.class.php
+++ core/modules/modCertibiocide.class.php
@@ -69,7 +69,7 @@ class modCertibiocide extends DolibarrModules
 		$this->editor_url = '';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.0';
+		$this->version = '1.1';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
@@ -80,7 +80,7 @@ class modCertibiocide extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		// To use a supported fa-xxx css style of font awesome, use this->picto='xxx'
-		$this->picto = 'fa-file-o';
+		$this->picto = 'certibiocide@certibiocide';
 
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
